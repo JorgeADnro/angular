05d8d5385f4d78394ff800664a1e8a82c49b6e04
JorgeADnro <106614143+JorgeADnro@users.noreply.github.com>
2023-02-08 11:36:17 -0600

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
+
+
+
 const routes: Routes = [
   {"path": '', component:AppComponent,pathMatch:"full"},
   {"path": 'login',component:LoginComponent,pathMatch:"full"},
